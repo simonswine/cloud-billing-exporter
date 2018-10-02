5f832be5f26915404059e8e3322ee29e6b44e2b3
Christian Simon <simon@swine.de>
2018-10-02 16:42:13 +0100

    Update godeps to latest tagged ones

--- vendor/cloud.google.com/go/internal/version/version.go
+++ vendor/cloud.google.com/go/internal/version/version.go
@@ -1,4 +1,4 @@
-// Copyright 2016 Google Inc. All Rights Reserved.
+// Copyright 2016 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,22 +19,53 @@
 package version
 
 import (
-	"regexp"
 	"runtime"
+	"strings"
+	"unicode"
 )
 
 // Repo is the current version of the client libraries in this
 // repo. It should be a date in YYYYMMDD format.
-const Repo = "20161214"
+const Repo = "20180226"
 
 // Go returns the Go runtime version. The returned string
 // has no whitespace.
 func Go() string {
-	return removeWhitespace(runtime.Version())
+	return goVersion
 }
 
-var whitespace = regexp.MustCompile(`\s`)
+var goVersion = goVer(runtime.Version())
 
-func removeWhitespace(s string) string {
-	return whitespace.ReplaceAllString(s, "_")
+const develPrefix = "devel +"
+
+func goVer(s string) string {
+	if strings.HasPrefix(s, develPrefix) {
+		s = s[len(develPrefix):]
+		if p := strings.IndexFunc(s, unicode.IsSpace); p >= 0 {
+			s = s[:p]
+		}
+		return s
+	}
+
+	if strings.HasPrefix(s, "go1") {
+		s = s[2:]
+		var prerelease string
+		if p := strings.IndexFunc(s, notSemverRune); p >= 0 {
+			s, prerelease = s[:p], s[p:]
+		}
+		if strings.HasSuffix(s, ".") {
+			s += "0"
+		} else if strings.Count(s, ".") < 2 {
+			s += ".0"
+		}
+		if prerelease != "" {
+			s += "-" + prerelease
+		}
+		return s
+	}
+	return ""
+}
+
+func notSemverRune(r rune) bool {
+	return strings.IndexRune("0123456789.", r) < 0
 }
